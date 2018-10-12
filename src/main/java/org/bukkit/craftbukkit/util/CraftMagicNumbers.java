36b2dc69ab56df95e74f904843c61f3c4b3fa428
Mgazul <admin@mgazul.cn>
2018-10-12 10:55:58 +0800

    修复Forge注册的方块ID和物品ID相同导致Bukkit无法正确处理

--- src/main/java/org/bukkit/craftbukkit/util/CraftMagicNumbers.java
+++ src/main/java/org/bukkit/craftbukkit/util/CraftMagicNumbers.java
@@ -44,7 +44,7 @@ public final class CraftMagicNumbers implements UnsafeValues {
     @Deprecated
     // A bad method for bad magic.
     public static Block getBlock(int id) {
-        return getBlock(Material.getMaterial(id));
+        return getBlock(Material.getBlockMaterial(id));
     }
 
     @Deprecated
@@ -54,7 +54,7 @@ public final class CraftMagicNumbers implements UnsafeValues {
     }
 
     public static Material getMaterial(Block block) {
-        return Material.getMaterial(Block.getIdFromBlock(block));
+        return Material.getBlockMaterial(Block.getIdFromBlock(block));
     }
 
     public static Item getItem(Material material) {
