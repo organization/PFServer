6a2b63f5aec913c36947cd7fb47c1a1f409e3aa7
Mgazul <mgazul@163.com>
2018-11-12 15:37:36 +0800

    简化指令/version

--- src/main/java/org/bukkit/command/defaults/VersionCommand.java
+++ src/main/java/org/bukkit/command/defaults/VersionCommand.java
@@ -4,8 +4,6 @@ import net.minecraftforge.common.ForgeVersion;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
-import java.util.Arrays;
-
 public class VersionCommand extends BukkitCommand {
     public VersionCommand(String name) {
         super(name);
@@ -13,18 +11,12 @@ public class VersionCommand extends BukkitCommand {
         this.description = "Gets the version of this server";
         this.usageMessage = "/version";
         this.setPermission("bukkit.command.version");
-        this.setAliases(Arrays.asList("ver"));
     }
 
     @Override
     public boolean execute(CommandSender sender, String currentAlias, String[] args) {
         if (!testPermission(sender)) return true;
-
-        if (args.length == 0) {
-            sender.sendMessage("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ", Forge version "+ForgeVersion.getVersion() + ")");
-        } else {
-            sender.sendMessage("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ", Forge version "+ForgeVersion.getVersion() + ")");
-        }
+        sender.sendMessage("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ", Forge version " + ForgeVersion.getVersion() + ")");
         return true;
     }
 }
