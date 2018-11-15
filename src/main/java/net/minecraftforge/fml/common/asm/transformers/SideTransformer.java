e060091c8191b92efa47595f05c2a4a3efa4f1e7
Mgazul <mgazul@163.com>
2018-11-15 18:40:25 +0800

    Update Forge to 14.23.5.2774

--- src/main/java/net/minecraftforge/fml/common/asm/transformers/SideTransformer.java
+++ src/main/java/net/minecraftforge/fml/common/asm/transformers/SideTransformer.java
@@ -82,14 +82,16 @@ public class SideTransformer implements IClassTransformer
             }
         }
 
-        // remove dynamic lambda methods that are inside of removed methods
-        List<Handle> dynamicLambdaHandles = lambdaGatherer.getDynamicLambdaHandles();
-        if (!dynamicLambdaHandles.isEmpty())
+        // remove dynamic synthetic lambda methods that are inside of removed methods
+        for (List<Handle> dynamicLambdaHandles = lambdaGatherer.getDynamicLambdaHandles();
+             !dynamicLambdaHandles.isEmpty(); dynamicLambdaHandles = lambdaGatherer.getDynamicLambdaHandles())
         {
             methods = classNode.methods.iterator();
             while (methods.hasNext())
             {
+                lambdaGatherer = new LambdaGatherer();
                 MethodNode method = methods.next();
+                if ((method.access & Opcodes.ACC_SYNTHETIC) == 0) continue;
                 for (Handle dynamicLambdaHandle : dynamicLambdaHandles)
                 {
                     if (method.name.equals(dynamicLambdaHandle.getName()) && method.desc.equals(dynamicLambdaHandle.getDesc()))
@@ -99,6 +101,7 @@ public class SideTransformer implements IClassTransformer
                             System.out.println(String.format("Removing Method: %s.%s%s", classNode.name, method.name, method.desc));
                         }
                         methods.remove();
+                        lambdaGatherer.accept(method);
                     }
                 }
             }
